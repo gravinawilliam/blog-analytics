23443beee132868804dae53f24201a9b2a4c0972
William Gravina <dev.gravinawilliam@gmail.com>
2021-10-14 20:53:18 -0300

    :poop: resetar esse commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+
+import { AllExceptionsFilter } from '@main/errors/all-exception.filter';
+
+import envConfig from './main/config/env.config';
+import { AppModule } from './main/modules/_global/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalFilters(new AllExceptionsFilter());
+  await app.listen(envConfig.port, () => {
+    if (envConfig.nodeEnv !== 'PROD') {
+      Logger.log(`✅ OK ${envConfig.port}`);
+    }
+  });
 }
 bootstrap();
